bab5b23ecfb889377d6a45589d2a3955c4a68da4
valeee13 <valentina.carta1987@gmail.com>
2018-05-04 11:08:06 +0200

    la parte del login

---
+++ CouseManagement/src/controller/ConnessioneSingleton.java
@@ -0,0 +1,57 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ConnessioneSingleton {
+
+	private Connection con; // oggetto che voglio creare in 'singleton'
+
+	private static ConnessioneSingleton istanza;
+
+	private ConnessioneSingleton() {
+		super();
+		// leggeiamo la nostra risorsa properties (bundle):
+		ResourceBundle rb = ResourceBundle.getBundle("utente.info.dbinfo");
+		String drv = rb.getString("drv");
+		String sid = rb.getString("sid");
+		String url = rb.getString("url");
+		String usr = rb.getString("usr");
+		String psw = rb.getString("psw");
+		String ssl = rb.getString("ssl");
+		try {
+			Class.forName(drv);
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
+		try {
+			con = DriverManager.getConnection(url + sid + ssl, usr, psw);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getCon() {
+		return con;
+	}
+
+	public static ConnessioneSingleton getIstanza() {
+		if (istanza == null)
+			istanza = new ConnessioneSingleton();
+		return istanza;
+	}
+
+	public static void chiudiEannullaStatico() {
+
+		// uso JDBC x chiudere la porta di connessione al DB':
+		try {
+			ConnessioneSingleton.getIstanza().con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		ConnessioneSingleton.istanza = null; // annullo questa istanza
+	}
+}
---
+++ CouseManagement/src/controller/LoginServlet.java
@@ -0,0 +1,92 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+			String confermaPass = request.getParameter("confermaPass");
+			
+			//registrazione utente
+			
+			UserDao ud = new UserDao();
+			User u = ud.selectByUsername(username);
+			
+			if (u!=null && u.getUsername().equals(username)) {
+				request.setAttribute("errorMsg3", "Username già presente!");
+				getServletContext().getRequestDispatcher("/jsp/registrazione.jsp")
+				.forward(request, response);
+			} else { 
+				if (password.equals(confermaPass)) {
+					if (password.equals("")) {
+						request.setAttribute("errorMsg4", " campo vuoto!");
+						getServletContext().getRequestDispatcher("/jsp/registrazione.jsp")
+						.forward(request, response);
+					} else {
+						request.setAttribute("username", request.getParameter("username"));
+						ud.insertUtente(username, password);
+						getServletContext().getRequestDispatcher("/jsp/benvenuto.jsp")
+						.forward(request, response);	
+					} 
+				} else {request.setAttribute("errorMsg5", "password errata!");
+				getServletContext().getRequestDispatcher("/jsp/registrazione.jsp")
+				.forward(request, response);
+					
+				}
+			}
+		}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		//verifica esistenza utente
+		
+		UserDao ud = new UserDao();
+		User u = ud.selectByUsername(username);	
+
+		if(u!=null && u.getUsername().equals(username)) {
+			if(u.getPassword().equals(password)) {
+				request.setAttribute("username", request.getParameter("username"));
+				getServletContext().getRequestDispatcher("/jsp/benvenuto.jsp")
+				.forward(request, response);
+
+				//benvenuto.jsp
+
+			} else {
+
+				request.setAttribute("erroreMsg1", " password errata!");
+				getServletContext().getRequestDispatcher("/jsp/home.jsp")
+				.forward(request, response);
+			}
+
+		} else {
+
+			request.setAttribute("erroreMsg2", "Utente inesistente! Registrati");
+			getServletContext().getRequestDispatcher("/jsp/registrazione.jsp")
+			.forward(request, response);
+
+			//registrazione.jsp
+
+		}
+	}
+
+
+}
+
+
